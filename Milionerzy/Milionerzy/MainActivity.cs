7e3d8664fbb8903dade0f66e5772a580498e5cd9
MichalPrzybyn <36730201+MichalPrzybyn@users.noreply.github.com>
2018-04-05 10:16:14 +0200

    ver. 1.0000000000000000001

--- Milionerzy/Milionerzy/MainActivity.cs
+++ Milionerzy/Milionerzy/MainActivity.cs
@@ -5,18 +5,65 @@ using Android.OS;
 namespace Milionerzy
 {
     [Activity(Label = "Milionerzy", MainLauncher = true)]
+    class glowny
+    {
+        
+        private string[] tab;
+        tab=new string[4]; 
+        private string pytanie;
+        private string odp1;
+        private string odp2;
+        private string odp3;
+        private string odp4;
+        private void Czytaj()
+        {
+
+        }
+        private void Zadaj()
+        {
+
+        }
+        private void Sprawdz()
+        {
+
+        }
+        private void Telefon()
+        {
+
+        }
+        private void fifty()
+        {
+
+        }
+        private void Publicznosc()
+        {
+
+        }
+
+
+
+
+
+    }
+
+
+
+
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
+           
             SetContentView(Resource.Layout.Main);
 
             
       
         }
     }
+
+
+
 }
 
