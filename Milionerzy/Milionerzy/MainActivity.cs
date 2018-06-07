f14ad0f823fc857888efc8dad3d2ff2da8f186bd
MichalPrzybyn <36730201+MichalPrzybyn@users.noreply.github.com>
2018-06-07 09:46:30 +0200

    finalna
    das

--- Milionerzy/Milionerzy/MainActivity.cs
+++ Milionerzy/Milionerzy/MainActivity.cs
@@ -31,17 +31,14 @@ namespace Milionerzy
         private Button mGra;
         private Button gra2a;
         private TextView mpytanko1;
-        private TextView mpytanko2;
+
         private TextView koniec;
         private ListView mpytanie1;
         private Button mOdpowiedzA1;
         private Button mOdpowiedzB1;
         private Button mOdpowiedzC1;
         private Button mOdpowiedzD1;
-        private Button mOdpowiedzA2;
-        private Button mOdpowiedzB2;
-        private Button mOdpowiedzC2;
-        private Button mOdpowiedzD2;
+
         private Button mPowrot;
         private List<string> mPytanie;
         private string plik;
@@ -49,27 +46,105 @@ namespace Milionerzy
         private string odpb1;
         private string odpc1;
         private string odpd1;
-        private string odpa2;
-        private string odpb2;
-        private string odpc2;
-        private string odpd2;
+
         private string content1;
-        private string content2;
+
         private int wynik;
+
+
+        public Button MGrajmy { get => mGrajmy; set => mGrajmy = value; }
+        public Button MRekord { get => mRekord; set => mRekord = value; }
+        public Button MWyjscie { get => mWyjscie; set => mWyjscie = value; }
+        public Button MGra { get => mGra; set => mGra = value; }
+        public Button Gra2a { get => gra2a; set => gra2a = value; }
+        public TextView Koniec { get => koniec; set => koniec = value; }
+        public ListView Mpytanie1 { get => mpytanie1; set => mpytanie1 = value; }
+        public Button MPowrot { get => mPowrot; set => mPowrot = value; }
+        public List<string> MPytanie { get => mPytanie; set => mPytanie = value; }
+        public string Plik { get => plik; set => plik = value; }
+        public string Odpa1 { get => odpa1; set => odpa1 = value; }
+        public string Odpb1 { get => odpb1; set => odpb1 = value; }
+        public string Odpc1 { get => odpc1; set => odpc1 = value; }
+        public string Odpd1 { get => odpd1; set => odpd1 = value; }
+        public int Wynik { get => wynik; set => wynik = value; }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            int licznik = 0;
+            int zdobycz = 0;
+            int x1;
+            Random rnd1 = new Random();
+            int n1;
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
 
             Button mGrajmy = FindViewById<Button>(Resource.Id.Grajmy);
-            
+
             mGrajmy.Click += (object sender, EventArgs args) =>
 
             {
                 {
-                    int licznik = 1;
-                    int zdobycz = 0;
+
+                    List<int> poprawne = new List<int>();
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+                    poprawne.Add(0);
+
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+                    poprawne.Add(0);
+
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+
+                    poprawne.Add(0);
+                    poprawne.Add(1);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
+                    poprawne.Add(0);
                     base.OnCreate(savedInstanceState);
                     SetContentView(Resource.Layout.gra1);
 
@@ -78,7 +153,7 @@ namespace Milionerzy
                     mOdpowiedzB1 = FindViewById<Button>(Resource.Id.odpB1);
                     mOdpowiedzC1 = FindViewById<Button>(Resource.Id.odpC1);
                     mOdpowiedzD1 = FindViewById<Button>(Resource.Id.odpD1);
-                    
+
                     AssetManager assets = this.Assets;
                     using (StreamReader sr = new StreamReader(assets.Open("pytanie1.txt")))
                     {
@@ -88,17 +163,14 @@ namespace Milionerzy
 
                     string[] str1 = content1.Split(new char[] { ',' });
 
-                    Random rnd1 = new Random();
-                    int n1 = rnd1.Next(0, 4);
-                    int x1 = 5 * n1;
 
-                   
-
+                    n1 = rnd1.Next(0, 10);
+                    x1 = 5 * n1;
                     string plik1 = str1[x1];
-                    string odpa1 = str1[x1+1];
-                    string odpb1 = str1[x1+2];
-                    string odpc1 = str1[x1+3];
-                    string odpd1 = str1[x1+4];
+                    string odpa1 = str1[x1 + 1];
+                    string odpb1 = str1[x1 + 2];
+                    string odpc1 = str1[x1 + 3];
+                    string odpd1 = str1[x1 + 4];
 
                     mpytanko1.Text = plik1;
                     mOdpowiedzA1.Text = odpa1;
@@ -109,35 +181,41 @@ namespace Milionerzy
                     mOdpowiedzA1.Click += (object sender2, EventArgs args2) =>
                     {
 
+                        if (poprawne[x1 + 1] == 1)
+                        {
+                            zdobycz++;
 
+                        }
                         Random rnd2 = new Random();
-                        int n2 = rnd2.Next(0, 4);
-                        int x2 = 0 + 5 * n2;
+                        int n2 = rnd2.Next(0, 10);
+                        x1 = 0 + 5 * n2;
 
 
-                        string plik2 = str1[x2];
-                        string odpa2 = str1[x2+1];
-                        string odpb2 = str1[x2+2];
-                        string odpc2 = str1[x2+3];
-                        string odpd2 = str1[x2+4];
+                        string plik2 = str1[x1];
+                        string odpa2 = str1[x1 + 1];
+                        string odpb2 = str1[x1 + 2];
+                        string odpc2 = str1[x1 + 3];
+                        string odpd2 = str1[x1 + 4];
 
                         mpytanko1.Text = plik2;
                         mOdpowiedzA1.Text = odpa2;
                         mOdpowiedzB1.Text = odpb2;
                         mOdpowiedzC1.Text = odpc2;
                         mOdpowiedzD1.Text = odpd2;
+
+
+
                         licznik++;
-                        zdobycz++;
                         if (licznik == 6)
                         {
                             base.OnCreate(savedInstanceState);
-                            
+
                             SetContentView(Resource.Layout.koniec);
                             Button mPowrot = FindViewById<Button>(Resource.Id.powrot);
 
 
                             TextView koniec = FindViewById<TextView>(Resource.Id.koniecc);
-                            string wynik = "Zdobyłeś " + zdobycz +"/6 PKT :)";
+                            string wynik = "Zdobyłeś " + zdobycz + "/" + licznik;
                             koniec.Text = wynik;
 
 
@@ -155,33 +233,44 @@ namespace Milionerzy
                     };
                     mOdpowiedzB1.Click += (object sender2, EventArgs args2) =>
                     {
+                        if (poprawne[x1 + 2] == 1)
+                        {
+                            zdobycz++;
+
+                        }
 
 
                         Random rnd2 = new Random();
-                        int n2 = rnd2.Next(0, 4);
-                        int x2 = 0 + 5 * n2;
+                        int n2 = rnd2.Next(0, 10);
+                        x1 = 0 + 5 * n2;
 
 
-                        string plik2 = str1[x2];
-                        string odpa2 = str1[x2 + 1];
-                        string odpb2 = str1[x2 + 2];
-                        string odpc2 = str1[x2 + 3];
-                        string odpd2 = str1[x2 + 4];
+                        string plik2 = str1[x1];
+                        string odpa2 = str1[x1 + 1];
+                        string odpb2 = str1[x1 + 2];
+                        string odpc2 = str1[x1 + 3];
+                        string odpd2 = str1[x1 + 4];
 
                         mpytanko1.Text = plik2;
                         mOdpowiedzA1.Text = odpa2;
                         mOdpowiedzB1.Text = odpb2;
                         mOdpowiedzC1.Text = odpc2;
                         mOdpowiedzD1.Text = odpd2;
+
+
+
+
+
                         licznik++;
+
                         if (licznik == 6)
                         {
                             base.OnCreate(savedInstanceState);
-                           
+
                             SetContentView(Resource.Layout.koniec);
                             Button mPowrot = FindViewById<Button>(Resource.Id.powrot);
                             TextView koniec = FindViewById<TextView>(Resource.Id.koniecc);
-                            string wynik = "Zdobyłeś " +zdobycz +"/6 PKT :)";
+                            string wynik = "Zdobyłeś " + zdobycz + "/" + licznik;
                             koniec.Text = wynik;
                             mPowrot.Click += (object sender4, EventArgs args4) =>
                             {
@@ -194,25 +283,35 @@ namespace Milionerzy
                     };
                     mOdpowiedzC1.Click += (object sender2, EventArgs args2) =>
                     {
+                        if (poprawne[x1 + 3] == 1)
+                        {
+                            zdobycz++;
 
+                        }
 
                         Random rnd2 = new Random();
-                        int n2 = rnd2.Next(0, 4);
-                        int x2 = 0 + 5 * n2;
+                        int n2 = rnd2.Next(0, 10);
+                        x1 = 0 + 5 * n2;
 
 
-                        string plik2 = str1[x2];
-                        string odpa2 = str1[x2 + 1];
-                        string odpb2 = str1[x2 + 2];
-                        string odpc2 = str1[x2 + 3];
-                        string odpd2 = str1[x2 + 4];
+                        string plik2 = str1[x1];
+                        string odpa2 = str1[x1 + 1];
+                        string odpb2 = str1[x1 + 2];
+                        string odpc2 = str1[x1 + 3];
+                        string odpd2 = str1[x1 + 4];
 
                         mpytanko1.Text = plik2;
                         mOdpowiedzA1.Text = odpa2;
                         mOdpowiedzB1.Text = odpb2;
                         mOdpowiedzC1.Text = odpc2;
                         mOdpowiedzD1.Text = odpd2;
+
+
+
+
+
                         licznik++;
+
                         if (licznik == 6)
                         {
                             base.OnCreate(savedInstanceState);
@@ -221,11 +320,11 @@ namespace Milionerzy
                             SetContentView(Resource.Layout.koniec);
                             Button mPowrot = FindViewById<Button>(Resource.Id.powrot);
                             TextView koniec = FindViewById<TextView>(Resource.Id.koniecc);
-                            string wynik = "Zdobyłeś " +zdobycz +"/6 PKT :)";
+                            string wynik = "Zdobyłeś " + zdobycz + "/" + licznik;
                             koniec.Text = wynik;
                             mPowrot.Click += (object sender4, EventArgs args4) =>
                             {
-                                
+
                                 base.OnCreate(savedInstanceState);
                                 SetContentView(Resource.Layout.Main);
 
@@ -235,16 +334,21 @@ namespace Milionerzy
                     };
                     mOdpowiedzD1.Click += (object sender2, EventArgs args2) =>
                     {
+                        if (poprawne[x1 + 4] == 1)
+                        {
+                            zdobycz++;
+
+                        }
                         Random rnd2 = new Random();
-                        int n2 = rnd2.Next(0, 4);
-                        int x2 = 0 + 5 * n2;
+                        int n2 = rnd2.Next(0, 10);
+                        x1 = 0 + 5 * n2;
 
 
-                        string plik2 = str1[x2];
-                        string odpa2 = str1[x2 + 1];
-                        string odpb2 = str1[x2 + 2];
-                        string odpc2 = str1[x2 + 3];
-                        string odpd2 = str1[x2 + 4];
+                        string plik2 = str1[x1];
+                        string odpa2 = str1[x1 + 1];
+                        string odpb2 = str1[x1 + 2];
+                        string odpc2 = str1[x1 + 3];
+                        string odpd2 = str1[x1 + 4];
 
                         mpytanko1.Text = plik2;
                         mOdpowiedzA1.Text = odpa2;
@@ -252,21 +356,25 @@ namespace Milionerzy
                         mOdpowiedzC1.Text = odpc2;
                         mOdpowiedzD1.Text = odpd2;
 
+
+
+
                         licznik++;
+
 
                     };
                     if (licznik == 6)
                     {
                         base.OnCreate(savedInstanceState);
-                        
+
                         SetContentView(Resource.Layout.koniec);
                         TextView koniec = FindViewById<TextView>(Resource.Id.koniecc);
-                        string wynik = "Zdobyłeś " +zdobycz +"/6 PKT :)";
+                        string wynik = "Zdobyłeś " + zdobycz + "/" + licznik;
                         koniec.Text = wynik;
                         Button mPowrot = FindViewById<Button>(Resource.Id.powrot);
                         mPowrot.Click += (object sender4, EventArgs args4) =>
                         {
-                             base.OnCreate(savedInstanceState);
+                            base.OnCreate(savedInstanceState);
                             SetContentView(Resource.Layout.Main);
 
                         };
@@ -280,23 +388,23 @@ namespace Milionerzy
 
             };
 
-
             Button mRekord = FindViewById<Button>(Resource.Id.rekord);
             mRekord.Click += delegate
             {
 
                 base.OnCreate(savedInstanceState);
-                
+
                 SetContentView(Resource.Layout.rekordy);
             };
+
 
             Button mWyjscie = FindViewById<Button>(Resource.Id.Wyjscie);
             mWyjscie.Click += delegate
             {
                 Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
             };
-           
-            
+
+
 
         }
 
--- Milionerzy/Milionerzy/obj/Release/android/src/milionerzy/milionerzy/R.java
+++ Milionerzy/Milionerzy/obj/Release/android/src/milionerzy/milionerzy/R.java
@@ -11,21 +11,43 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int image=0x7f020000;
+        public static final int icon=0x7f020000;
+        public static final int image=0x7f020001;
     }
     public static final class id {
-        public static final int MyButton=0x7f050002;
-        public static final int button1=0x7f050003;
-        public static final int button2=0x7f050004;
-        public static final int imageDisplay=0x7f050001;
-        public static final int linearLayout1=0x7f050000;
-        public static final int muzyka=0x7f050006;
-        public static final int myListView=0x7f050005;
+        public static final int Gra1=0x7f050015;
+        public static final int Grajmy=0x7f05000f;
+        public static final int Imie=0x7f050014;
+        public static final int Wyjscie=0x7f050011;
+        public static final int imageDisplay=0x7f05000e;
+        public static final int koniecc=0x7f05000b;
+        public static final int licznik=0x7f050005;
+        public static final int linearLayout1=0x7f05000d;
+        public static final int muzyka=0x7f050012;
+        public static final int odpA1=0x7f050001;
+        public static final int odpA2=0x7f050007;
+        public static final int odpB1=0x7f050002;
+        public static final int odpB2=0x7f050008;
+        public static final int odpC1=0x7f050003;
+        public static final int odpC2=0x7f050009;
+        public static final int odpD1=0x7f050004;
+        public static final int odpD2=0x7f05000a;
+        public static final int powrot=0x7f05000c;
+        public static final int pytanko1=0x7f050000;
+        public static final int pytanko2=0x7f050006;
+        public static final int rekord=0x7f050010;
+        public static final int textView1=0x7f050013;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int gra1=0x7f030000;
+        public static final int gra2=0x7f030001;
+        public static final int koniec=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int pimie=0x7f030004;
+        public static final int rekordy=0x7f030005;
     }
     public static final class string {
+        public static final int ReadTextFile=0x7f040001;
         public static final int app_name=0x7f040000;
     }
 }
